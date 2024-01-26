9dae92036ce0bfa829d9f817a40aad8348fc2316
sodaakim <sohuikim2020@naver.com>
2024-01-27 02:43:31 +0900

    made evaluation page

--- app.py
+++ app.py
@@ -3,19 +3,6 @@ from flask_mail import Mail, Message
 from itsdangerous import URLSafeTimedSerializer, SignatureExpired
 
 app = Flask(__name__)
-
-# Flask-Mail 설정
-app.config['MAIL_SERVER'] = 'smtp.example.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'your-email@example.com'
-app.config['MAIL_PASSWORD'] = 'your-password'
-app.config['SECRET_KEY'] = 'your-secret-key'  # 시크릿 키 설정
-mail = Mail(app)
-
-# URLSafeTimedSerializer 인스턴스 생성
-s = URLSafeTimedSerializer(app.config['SECRET_KEY'])
-
 @app.route('/')
 def home():
     cards = [
@@ -23,7 +10,6 @@ def home():
         {'title': 'Re-engineered modules and batch evaluation support', 'subtitle': 'Sub 1', 'description': 'Based on the studies conducted in papers, we have developed a model capable of accurately predicting ADMET properties. Additionally, this model demonstrates excellent performance in predicting various toxicities, proving its robustness and reliability in the field of pharmacological research.', 'image_path': 'img/path1.jpg'},
         {'title': 'Robust and accurate MGA models', 'subtitle': 'Sub 2', 'description': 'Bproperties. Additionally, this model demonstrates excellent performance in predicting various toxicities, proving its robustness and reliability in the field of pharmacological research.', 'image_path': 'img/path2.jpg'},
         {'title': 'Practical explanation and guidance', 'subtitle': 'Sub 2', 'description': 'Bproperties. Additionally, this model demonstrates excellent performance in predicting various toxicities, proving its robustness and reliability in the field of pharmacological research.', 'image_path': 'img/path2.jpg'},
-        # ... 추가 카드 데이터
     ]
     return render_template('home.html', cards=cards)
 
@@ -40,61 +26,105 @@ def analyze():
     ]
     return render_template('analyze.html', steps=steps)
 
+@app.route('/screening')
+def screening():
+    steps = [
+        {'title': 'Step 4', 'description': 'paste admet1'},
+        {'title': 'Step 5', 'description': 'paste admet2'},
+        {'title': 'Step 6', 'description': 'paste admsdsdsdsdsd'}
+    ]
+    cardsA = [
+        {'img': 'url', 'smiles': 'CC(C)OC(=O)CC(=O)CSC1=C(C=C2CCCC2=N1)C#N'},
+        {'img': 'url', 'smiles': 'CC(C)OC(=O)CC(=O)CSC1=C(C=C2CCCC2=N1)C#N'},
+        {'img': 'url', 'smiles': 'CC(C)OC(=O)CC(=O)CSC1=C(C=C2CCCC2=N1)C#N'},
+        {'img': 'url', 'smiles': 'CC(C)OC(=O)CC(=O)CSC1=C(C=C2CCCC2=N1)C#N'},
+        {'img': 'url', 'smiles': 'CC(C)OC(=O)CC(=O)CSC1=C(C=C2CCCC2=N1)C#N'},
+        {'img': 'url', 'smiles': 'CC(C)OC(=O)CC(=O)CSC1=C(C=C2CCCC2=N1)C#N'}
+    ]
+    cardsI = [
+        {'img': 'url', 'smiles': 'CC(C)OC(=O)CC(=O)CSC1=C(C=C2CCCC2=N1)C#N'},
+        {'img': 'url', 'smiles': 'CC(C)OC(=O)CC(=O)CSC1=C(C=C2CCCC2=N1)C#N'},
+        {'img': 'url', 'smiles': 'CC(C)OC(=O)CC(=O)CSC1=C(C=C2CCCC2=N1)C#N'},
+        {'img': 'url', 'smiles': 'CC(C)OC(=O)CC(=O)CSC1=C(C=C2CCCC2=N1)C#N'},
+        {'img': 'url', 'smiles': 'CC(C)OC(=O)CC(=O)CSC1=C(C=C2CCCC2=N1)C#N'},
+        {'img': 'url', 'smiles': 'CC(C)OC(=O)CC(=O)CSC1=C(C=C2CCCC2=N1)C#N'}
+    ]
+    return render_template('screening.html', steps=steps, cardsA=cardsA, cardsI=cardsI)
+
 @app.route('/example')
 def example():
     return render_template('example.html')
 
 @app.route('/signupform')
 def signupform():
-    return  render_template('signup.html')
+    return render_template('signup.html')
 
 @app.route('/evaluation')
 def evaluation():
-    return  render_template('evaluation.html')
+    properties = [ # property
+        # content : Physicochemical Property
+        {'title': 'Physicochemical Properties',
+         'contents': [
+            {'subtitle': 'Formula',                 'num': 'num', 'color': '#103B59'},
+            {'subtitle': 'Molecular Weight',        'num': 'num', 'color': '#103B59'},
+            {'subtitle': 'Volume',                  'num': 'num', 'color': '#103B59'},
+            {'subtitle': 'Density',                 'num': 'num', 'color': '#103B59'},
+            {'subtitle': 'Num. heavy atoms',        'num': 'num', 'color': '#103B59'},
+            {'subtitle': 'Num. arom. heavy atoms',  'num': 'num', 'color': '#103B59'},
+            {'subtitle': 'formal Charge',           'num': 'num', 'color': '#103B59'},
+            {'subtitle': 'TPSA',                    'num': 'num', 'color': '#103B59'},
+            {'subtitle': 'logS',                    'num': 'num', 'color': '#103B59'},
+            {'subtitle': 'logP',                    'num': 'num', 'color': '#103B59'},
+            {'subtitle': 'logD',                    'num': 'num', 'color': '#103B59'},
+            {'subtitle': 'DMSO',                    'num': 'num', 'color': '#103B59'},
+            {'subtitle': 'nHA',                     'num': 'num', 'color': '#103B59'},
+            {'subtitle': 'nHD',                     'num': 'num', 'color': '#103B59'},
+            {'subtitle': 'nRot',                    'num': 'num', 'color': '#103B59'},
+            {'subtitle': 'nRing',                   'num': 'num', 'color': '#103B59'},
+            {'subtitle': 'nHet',                    'num': 'num', 'color': '#103B59'},
+            {'subtitle': 'nRig',                    'num': 'num', 'color': '#103B59'},
+            {'subtitle': 'pKa',                     'num': 'num', 'color': '#103B59'}
+        ]},
+        # content : Medicinal Chemistry
+        {'title': 'Medicinal Chemistry',
+         'contents': [
+            {'subtitle': 'SAscore',                 'num': 'num', 'color': '#103B59'},
+            {'subtitle': 'NPscore',                 'num': 'num', 'color': '#103B59'},
+            {'subtitle': 'Lipinski Rule',           'num': 'num', 'color': '#103B59'},
+            {'subtitle': 'Pfizer Rule',             'num': 'num', 'color': '#103B59'},
+            {'subtitle': 'Ghose',                   'num': 'num', 'color': '#103B59'},
+            {'subtitle': 'GSK Rule',                'num': 'num', 'color': '#103B59'},
+            {'subtitle': 'Veber (GSK) filter',   'num': 'num', 'color': '#103B59'},
+            {'subtitle': 'Golden Triangle',         'num': 'num', 'color': '#103B59'},
+            {'subtitle': 'PAINS',                   'num': 'num', 'color': '#103B59'},
+            {'subtitle': 'Leadlikeness',            'num': 'num', 'color': '#103B59'},
+            {'subtitle': 'Brenk',                   'num': 'num', 'color': '#103B59'}
+        ]},
+        # content : Druglikeness
+        {'title': 'Druglikeness',
+         'contents': [
+            {'subtitle': 'Egan',                    'num': 'num', 'color': '#103B59'},
+            {'subtitle': 'Bioavailability Score',   'num': 'num', 'color': '#103B59'}
+        ]},
+        # content : Absorption
+        {'title': 'Absorption',
+         'contents': [
+            {'subtitle': 'Caco-2 Permeability',     'num': 'num', 'color': '#103B59'},
+            {'subtitle': 'MDCK Permeability',       'num': 'num', 'color': '#103B59'},
+            {'subtitle': 'Pgp-inhibitor',           'num': 'num', 'color': '#103B59'},
+            {'subtitle': 'Pgp-substrate',           'num': 'num', 'color': '#103B59'},
+            {'subtitle': 'HIA',                     'num': 'num', 'color': '#103B59'}
+        ]},
+        # content : Distribution
+        {'title': 'Title6',
+         'contents': [
+            {'subtitle': 'AA', 'num': 'AAA', 'color': '#103B59'},
+            {'subtitle': 'BB', 'num': 'BBB', 'color': '#103B59'},
+            {'subtitle': 'BB', 'num': 'BBB', 'color': '#103B59'}
+        ]}
+    ]
+    return render_template('evaluation.html', properties=properties)
 
-
-@app.route('/signup', methods=['POST'])
-
-def signup():
-    if request.method == 'POST':
-
-        email = request.json.get('email')
-        password = request.json.get('password')
-
-        # Validate email format
-        if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
-            return jsonify(message="Invalid email format."), 400
-
-        # Hash the password for secure storage
-        # hashed_password = generate_password_hash(password)
-
-        # Implement your signup logic here, e.g., save user to database
-        # ...
-
-        # Email confirmation token and link
-        token = s.dumps(email, salt='email-confirm')
-        confirm_url = url_for('confirm_email', token=token, _external=True)
-
-        # Send confirmation email
-        msg = Message("Please confirm your email", sender='your-email@example.com', recipients=[email])
-        msg.body = f"Please click on the link to confirm your email: {confirm_url}"
-        mail.send(msg)
-
-        return jsonify(message="Please check your email to confirm your registration."), 200
-    else:
-        return 0 #render_template('signup.html')
-
-@app.route('/confirm/<token>')
-def confirm_email(token):
-    try:
-        email = s.loads(token, salt='email-confirm', max_age=3600)
-    except SignatureExpired:
-        return jsonify(message="The confirmation link is expired."), 400
-
-    # Implement your token confirmation logic here
-    # ...
-
-    return jsonify(message="Your email has been confirmed."), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
