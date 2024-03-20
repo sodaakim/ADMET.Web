9d6b15a85027eb45494365cbe79b4f9d4017479f
sodaakim <sohuikim2020@naver.com>
2024-03-20 09:51:57 +0900

    add pages

--- reference.py
+++ reference.py
@@ -11,13 +11,13 @@ reference_route = Blueprint('reference_route', __name__)
 @reference_route.route('/reference')
 def reference():
     data_models = [
-        {'title': 'Model Example1', 'image_url': 'image', 'description': 'This model is about..', 'link': 'Link'},
-        {'title': 'Model Example2', 'image_url': 'image', 'description': 'This model is about..', 'link': 'Link'}
+        {'title': 'Model Example1', 'image_url': 'static/resources/reference.png', 'description': 'This model is about..', 'link': 'Link'},
+        {'title': 'Model Example2', 'image_url': 'static/resources/reference.png', 'description': 'This model is about..', 'link': 'Link'}
     ]
 
     parameters = [
-        {'title': 'parameters Example1', 'image_url': 'image', 'description': 'parameters..', 'link': 'Link'},
-        {'title': 'parameters Example2', 'image_url': 'image', 'description': 'parameters..', 'link': 'Link'}
+        {'title': 'parameters Example1', 'image_url': 'static/resources/reference.png', 'description': 'parameters..', 'link': 'Link'},
+        {'title': 'parameters Example2', 'image_url': 'static/resources/reference.png', 'description': 'parameters..', 'link': 'Link'}
     ]
     return render_template('reference.html', data_models=data_models, parameters=parameters)
 
@@ -25,12 +25,12 @@ def reference():
 @reference_route.route('/ssbio_lab')
 def ssbio_lab():
     lab_projects = [
-        {'title': 'lab projects Example1', 'image_url': 'image', 'description': 'This model is about..', 'link': 'Link'},
-        {'title': 'lab projects Example2', 'image_url': 'image', 'description': 'This model is about..', 'link': 'Link'}
+        {'title': 'lab projects Example1', 'image_url': 'static/resources/reference.png', 'description': 'This model is about..', 'link': 'Link'},
+        {'title': 'lab projects Example2', 'image_url': 'static/resources/reference.png', 'description': 'This model is about..', 'link': 'Link'}
     ]
     research_themes = [
-        {'title': 'research themes Example1', 'image_url': 'image', 'description': 'This model is about..', 'link': 'Link'},
-        {'title': 'research themes Example2', 'image_url': 'image', 'description': 'This model is about..', 'link': 'Link'}
+        {'title': 'research themes Example1', 'image_url': 'static/resources/reference.png', 'description': 'This model is about..', 'link': 'Link'},
+        {'title': 'research themes Example2', 'image_url': 'static/resources/reference.png', 'description': 'This model is about..', 'link': 'Link'}
     ]
     return render_template('ssbio_lab.html', lab_projects=lab_projects, research_themes=research_themes)
 
